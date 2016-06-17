71b5c502da8aafa146bd3deed7c133175483c61b
Amy Chen <amchen@adobe.com>
2016-06-17 17:26:28 -0400

    aksjhdkjsad

--- documentation.sh
+++ documentation.sh
@@ -45,7 +45,7 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
   ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
   ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-  openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+  openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in id_rsa_travisTest.enc -out id_rsa_travisTest -d
   chmod 600 deploy_key
   eval `ssh-agent -s`
   ssh-add deploy_key
