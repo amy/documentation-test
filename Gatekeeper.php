3835d23a1ea2d6c94cdba50a5998074534fbd93e
Amy Chen <amchen@adobe.com>
2016-06-17 17:07:51 -0400

    test

--- Gatekeeper.php
+++ Gatekeeper.php
@@ -7,7 +7,7 @@ use Behance\NBD\Gatekeeper\Rules\PercentageRule;
 class Gatekeeper {
 
   /**
-   * @var \Behance\NBD\Gatekeeper\RulesetProviderInterface
+   * @var \Behance\NBD\Gatekeeper\RulesetProviderInterfaceTESTTESTTEST
    */
   private $_ruleset_provider;
 
